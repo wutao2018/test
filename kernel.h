64320433f2213552407686eb46edb6c4db992fbe
wutao2018 <55196994+wutao2018@users.noreply.github.com>
2021-06-09 20:48:17 +0800

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -60,6 +60,7 @@ __global__ void gemm<256>(int M[], int N[], int K[], float *A[], float *B[], flo
 				gemm_256_32x32(M[i], N[i], K[i], A[i], B[i], C[i], sh);
 				//gemm_256_32x32_mdb(M[i], N[i], K[i], A[i], B[i], C[i], sh);
 				//gemm_256_32x32_nrb(M[i], N[i], K[i], A[i], B[i], C[i], sh);
+				//gemm_256_32x32_vl(M[i], N[i], K[i], A[i], B[i], C[i], sh);
 			break;
 		case 2:
 			if (blockIdx.x * 64 < M[i] && blockIdx.y * 64 < N[i])	
