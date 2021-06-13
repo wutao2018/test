decf5d0e16e82c1809c9a73d7dccdc44ccd1e9ee
wutao2018 <55196994+wutao2018@users.noreply.github.com>
2021-06-13 20:28:34 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -13,31 +13,49 @@ do
 
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 8 >> log
 	done
 	
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 16 >> log
 	done
 
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 32 >> log
 	done	
 
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 64 >> log
 	done
 	
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 128 >> log
 	done
 
 	for ((K=16; K<=1024; K=K*2))
 	do
+		cd ../data
+		./gen_data $M $M $K
+		cd - > /dev/null	
 		./gemm 256 >> log00
 	done
 done
