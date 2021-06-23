31f456f5d5ec7db706eb33d5ffee1400d17fa4c4
wutao2018 <55196994+wutao2018@users.noreply.github.com>
2021-06-23 10:31:22 +0800

    Update fp16.cu

--- fp16.cu
+++ fp16.cu
@@ -1228,7 +1228,196 @@ __global__ void gemm_256_64x128_16(int M, int N, int K, float *A, float *B, floa
 
 
 
-int main(int argc, char* argv[]) {
+__device__ void gemm_256_128x128(int M, int N, int K, float *A, float *B, float *C, float *sh){
+
+    __shared__ float sh_A[2048];
+	__shared__ float sh_A[2048];
+	//float sh_B = sh + 2*128*8;
+
+	float4 reg_C[16];
+	float reg_A[8];
+	float reg_B[8];
+
+	// Compute block's starting coordinate
+	int block_base_x = blockIdx.y*128;
+	int block_base_y = blockIdx.x*128;
+
+	//Load C from global memory to register file
+	float4 *C_start = (float4*) (C + block_base_x*M + block_base_y + (threadIdx.x%16)*4 + (threadIdx.x/16)*4*M);
+
+    reg_C[0] = {0.f,0.f,0.f,0.f};//*C_start;
+	reg_C[1] = {0.f,0.f,0.f,0.f};//*(C_start + md4);
+	reg_C[2] = {0.f,0.f,0.f,0.f};//*(C_start + md2);
+	reg_C[3] = {0.f,0.f,0.f,0.f};//*(C_start + 3*md4);  
+
+	C_start += 16;
+	reg_C[4] = {0.f,0.f,0.f,0.f};//*(C_start);
+	reg_C[5] = {0.f,0.f,0.f,0.f};//*(C_start + md4);
+	reg_C[6] = {0.f,0.f,0.f,0.f};//*(C_start + md2);
+	reg_C[7] = {0.f,0.f,0.f,0.f};//*(C_start + 3*md4);
+
+	C_start += (m16 - 16);
+	reg_C[8] = {0.f,0.f,0.f,0.f};//*(C_start);
+	reg_C[9] = {0.f,0.f,0.f,0.f};//*(C_start + md4);
+	reg_C[10] = {0.f,0.f,0.f,0.f};//*(C_start + md2);
+	reg_C[11] = {0.f,0.f,0.f,0.f};//*(C_start + 3*md4);
+
+	C_start += 16;
+	reg_C[12] = {0.f,0.f,0.f,0.f};//*(C_start);
+	reg_C[13] = {0.f,0.f,0.f,0.f};//*(C_start + md4);
+	reg_C[14] = {0.f,0.f,0.f,0.f};//*(C_start + md2);
+	reg_C[15] = {0.f,0.f,0.f,0.f};//*(C_start + 3*md4);
+
+	//load A from global memory to shared memory
+	float4 *A_start = (float4*) (A + block_base_y + (threadIdx.x%32)*4 + (threadIdx.x/32)*M); 
+	*((float4*) (sh_A + 4*threadIdx.x)) = *(A_start);
+
+	//load A from global memory to shared memory
+	float4 *B_start = (float4*) (B + K*block_base_x + (threadIdx.x/128)*4 + (threadIdx.x%128)*K); 
+	*((float4*) (sh_B + 4*threadIdx.x)) = *(B_start);
+		
+	int double_buffer = 0;
+#pragma unroll
+	for(int k=0; k<K; k+=8){
+
+		__syncthreads();
+		int A_offset = double_buffer + (threadIdx.x%16)*4;
+		int B_offset = double_buffer + ((threadIdx.x/16)*16);
+			
+#pragma unroll
+		for (int i=0; i<8; ++i)	{
+			
+			reg_A[0] = sh_A[A_offset];
+			reg_A[1] = sh_A[A_offset+1];
+			reg_A[2] = sh_A[A_offset+2];
+			reg_A[3] = sh_A[A_offset+3];
+			reg_A[4] = sh_A[A_offset+64];
+			reg_A[5] = sh_A[A_offset+65];
+			reg_A[6] = sh_A[A_offset+66];
+			reg_A[7] = sh_A[A_offset+67];
+
+			reg_B[0] = sh_B[B_offset];
+			reg_B[1] = sh_B[B_offset+4];
+			reg_B[2] = sh_B[B_offset+8];
+			reg_B[3] = sh_B[B_offset+12];
+			reg_B[4] = sh_B[B_offset+256];
+			reg_B[5] = sh_B[B_offset+260];
+			reg_B[6] = sh_B[B_offset+264];
+			reg_B[7] = sh_B[B_offset+268];
+
+			reg_C[0].x = fma(reg_A[0], reg_B[0], reg_C[0].x);
+			reg_C[1].x = fma(reg_A[0], reg_B[1], reg_C[1].x);
+			reg_C[2].x = fma(reg_A[0], reg_B[2], reg_C[2].x);
+			reg_C[3].x = fma(reg_A[0], reg_B[3], reg_C[3].x);
+			reg_C[8].x = fma(reg_A[0], reg_B[4], reg_C[8].x);
+			reg_C[9].x = fma(reg_A[0], reg_B[5], reg_C[9].x);
+			reg_C[10].x = fma(reg_A[0], reg_B[6], reg_C[10].x);
+			reg_C[11].x = fma(reg_A[0], reg_B[7], reg_C[11].x);
+			reg_C[4].x = fma(reg_A[4], reg_B[0], reg_C[4].x);
+			reg_C[5].x = fma(reg_A[4], reg_B[1], reg_C[5].x);
+			reg_C[6].x = fma(reg_A[4], reg_B[2], reg_C[6].x);
+			reg_C[7].x = fma(reg_A[4], reg_B[3], reg_C[7].x);
+			reg_C[12].x = fma(reg_A[4], reg_B[4], reg_C[12].x);
+			reg_C[13].x = fma(reg_A[4], reg_B[5], reg_C[13].x);
+			reg_C[14].x = fma(reg_A[4], reg_B[6], reg_C[14].x);
+			reg_C[15].x = fma(reg_A[4], reg_B[7], reg_C[15].x);
+
+			reg_C[0].y = fma(reg_A[1], reg_B[0], reg_C[0].y);
+			reg_C[1].y = fma(reg_A[1], reg_B[1], reg_C[1].y);
+			reg_C[2].y = fma(reg_A[1], reg_B[2], reg_C[2].y);
+			reg_C[3].y = fma(reg_A[1], reg_B[3], reg_C[3].y);
+			reg_C[8].y = fma(reg_A[1], reg_B[4], reg_C[8].y);
+			reg_C[9].y = fma(reg_A[1], reg_B[5], reg_C[9].y);
+			reg_C[10].y = fma(reg_A[1], reg_B[6], reg_C[10].y);
+			reg_C[11].y = fma(reg_A[1], reg_B[7], reg_C[11].y);
+			reg_C[4].y = fma(reg_A[5], reg_B[0], reg_C[4].y);
+			reg_C[5].y = fma(reg_A[5], reg_B[1], reg_C[5].y);
+			reg_C[6].y = fma(reg_A[5], reg_B[2], reg_C[6].y);
+			reg_C[7].y = fma(reg_A[5], reg_B[3], reg_C[7].y);
+			reg_C[12].y = fma(reg_A[5], reg_B[4], reg_C[12].y);
+			reg_C[13].y = fma(reg_A[5], reg_B[5], reg_C[13].y);
+			reg_C[14].y = fma(reg_A[5], reg_B[6], reg_C[14].y);
+			reg_C[15].y = fma(reg_A[5], reg_B[7], reg_C[15].y);
+
+			reg_C[0].z = fma(reg_A[2], reg_B[0], reg_C[0].z);
+			reg_C[1].z = fma(reg_A[2], reg_B[1], reg_C[1].z);
+			reg_C[2].z = fma(reg_A[2], reg_B[2], reg_C[2].z);
+			reg_C[3].z = fma(reg_A[2], reg_B[3], reg_C[3].z);
+			reg_C[8].z = fma(reg_A[2], reg_B[4], reg_C[8].z);
+			reg_C[9].z = fma(reg_A[2], reg_B[5], reg_C[9].z);
+			reg_C[10].z = fma(reg_A[2], reg_B[6], reg_C[10].z);
+			reg_C[11].z = fma(reg_A[2], reg_B[7], reg_C[11].z);
+			reg_C[4].z = fma(reg_A[6], reg_B[0], reg_C[4].z);
+			reg_C[5].z = fma(reg_A[6], reg_B[1], reg_C[5].z);
+			reg_C[6].z = fma(reg_A[6], reg_B[2], reg_C[6].z);
+			reg_C[7].z = fma(reg_A[6], reg_B[3], reg_C[7].z);
+			reg_C[12].z = fma(reg_A[6], reg_B[4], reg_C[12].z);
+			reg_C[13].z = fma(reg_A[6], reg_B[5], reg_C[13].z);
+			reg_C[14].z = fma(reg_A[6], reg_B[6], reg_C[14].z);
+			reg_C[15].z = fma(reg_A[6], reg_B[7], reg_C[15].z);
+
+			reg_C[0].w = fma(reg_A[3], reg_B[0], reg_C[0].w);
+			reg_C[1].w = fma(reg_A[3], reg_B[1], reg_C[1].w);
+			reg_C[2].w = fma(reg_A[3], reg_B[2], reg_C[2].w);
+			reg_C[3].w = fma(reg_A[3], reg_B[3], reg_C[3].w);
+			reg_C[8].w = fma(reg_A[3], reg_B[4], reg_C[8].w);
+			reg_C[9].w = fma(reg_A[3], reg_B[5], reg_C[9].w);
+			reg_C[10].w = fma(reg_A[3], reg_B[6], reg_C[10].w);
+			reg_C[11].w = fma(reg_A[3], reg_B[7], reg_C[11].w);
+			reg_C[4].w = fma(reg_A[7], reg_B[0], reg_C[4].w);
+			reg_C[5].w = fma(reg_A[7], reg_B[1], reg_C[5].w);
+			reg_C[6].w = fma(reg_A[7], reg_B[2], reg_C[6].w);
+			reg_C[7].w = fma(reg_A[7], reg_B[3], reg_C[7].w);
+			reg_C[12].w = fma(reg_A[7], reg_B[4], reg_C[12].w);
+			reg_C[13].w = fma(reg_A[7], reg_B[5], reg_C[13].w);
+			reg_C[14].w = fma(reg_A[7], reg_B[6], reg_C[14].w);
+			reg_C[15].w = fma(reg_A[7], reg_B[7], reg_C[15].w);
+
+			A_offset += 128;
+			if (i==3) B_offset += 508;
+			B_offset += 1;
+		}
+
+		double_buffer ^= 1024;
+
+		if (k+8 < K){
+			A_start += 2*M; 
+			*((float4*) (sh_A + double_buffer + 4*threadIdx.x)) = *(A_start);
+
+			B_start += 2; 
+			*((float4*) (sh_B + double_buffer + 4*threadIdx.x)) = *(B_start);
+		}
+				
+	}
+	
+	C_start -= (16*M + 16);
+    *C_start = reg_C[0];
+	*(C_start + M/4) = reg_C[1];
+	*(C_start + M/2) = reg_C[2];
+	*(C_start + 3*M/4) = reg_C[3];
+
+	C_start += 16;
+	*(C_start) = reg_C[4];
+	*(C_start + M/4) = reg_C[5];
+	*(C_start + M/2) = reg_C[6];
+	*(C_start + 3*M/4) = reg_C[7];
+
+	C_start += (16*M - 16);
+	*(C_start) = reg_C[8];
+	*(C_start + M/4) = reg_C[9];
+	*(C_start + M/2) = reg_C[10];
+	*(C_start + 3*M/4) = reg_C[11];
+
+	C_start += 16;
+	*(C_start) = reg_C[12];
+	*(C_start + M/4) = reg_C[13];
+	*(C_start + M/2) = reg_C[14];
+	*(C_start + 3*M/4) = reg_C[15];
+}
+
+
+int main(int argc, char* argv[]) 
+{
    float *a_fp32;
    float *b_fp32;
    half *a_fp16;
